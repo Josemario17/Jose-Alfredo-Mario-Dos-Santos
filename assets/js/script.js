57b9888ef05cc4953fa7dc8133aa3b27a72000d6
Josy dos Santos <josydossantos2002@gmail.com>
2024-02-17 22:16:24 +0100

    nova versao

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,18 @@
 document.querySelector("#click-to-top").onclick = function(){
     document.querySelector("body").scrollIntoView();
 }
+
+//type submit function
+const sendMessage = document.getElementById("sendMessage")
+let requiridElements = document.querySelectorAll("form *[required]")
+
+sendMessage.addEventListener("submit", (e)=>{
+    e.preventDefault()
+    document.getElementById("messageSuccess").classList.remove("display-none")
+    for (let index = 0; index < requiridElements.length; index++) {
+        requiridElements[index].value = ""
+    }
+    setTimeout(() => {    
+        document.getElementById("messageSuccess").classList.add("display-none")
+    }, 2500);
+})
